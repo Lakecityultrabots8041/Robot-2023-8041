238f4cc94a4bbea6416bfb0555e07a726d1b1a22
lakecityultrabots8041 <lcrobotics@lakecitytrojans.org>
2023-03-01 08:15:09 -0500

    PIdgains

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,45 +4,162 @@
 
 package frc.robot;
 
+//import frc.robot.utils.PIDGains;
+//import frc.robot.utils.TalonConstants;
+
 public class Constants {;
   
   public class IDs {
     //Ids of Victors 
     public class Victor {
-      public static final int driveLeftrear = 1;
-      public static final int driveLeftfront = 2;
-      public static final int driveRightrear = 3;
-      public static final int driveRightfront = 4;
-
-
-    
+      public static final int driveLeftrearId = 1;
+      public static final int driveLeftfrontId = 2;
+      public static final int driveRightrearId = 3;
+      public static final int driveRightfrontId = 4;
+      public static final int armControllerId2 = 8;
     }
-// Ids of TalonSRX
-  public class TalonSRX  {
-     public static final int driveTopLeft = 5;
-     public static final int driveTopRight = 6; 
-  }
+  
+    // Ids of TalonSRX
+    public class TalonSRX  {
+      public static final int driveTopLeftId = 5;
+      public static final int driveTopRightId = 6; 
+      public static final int armControllerId1 = 7;
+      public static final int armControllerId2 = 8;
+      public static final int armExtendControllerId = 9;
+    }
+
+    //Solenoids
+    public class Air {
+      public static final int driveShiftHighId = 0;
+      public static final int driveShiftLowId = 1;
+      public static final int gripperSolenoidId = 2;
+    }
  
-    } //DriveTrain 
+    } 
+    
+    //DriveTrain 
     public class DriveTrain {
-      public static final double speedmult = 0.30;// slow down math boy 
+      public static final double kSpeedMult = 1.0; // slow down math boy 
+      public static final double kTurnMult = 1.0; //reduce rate of turn
       public class Left{
         public static final boolean isInverted = true; //invert left side
       } 
       public class Right {
-        public static final boolean isInverted = true ;
+        public static final boolean isInverted = !Left.isInverted;
       }
-    
-    //Operator Interface
-   
-  }
-  public class OI{
-    public static final int Joy = 0;
-    public static final double maxband = 0.90;
-    public static final double minband = 0.10;
   }
 
-}  
+  //Gripper Constants
+  public class Gripper {
+    public static final boolean kOpenState = true; //state of the 
+  }
+
+  public class Arm {
+    public class Positions { 
+      public static final double ScoreHigh = 5000;
+      public static final double ScoreLow = 1000;
+      public static final double HPShelf = 3000;
+      public static final double Floor = 1000;
+      public static final double Home = 0;
+    }
+    public class Motor1 {
+      //Talon Tuning
+      public static final int motorId = IDs.TalonSRX.armControllerId1;
+      public static final boolean isInverted = true;
+      public static final boolean sensorPhase = false;
+      // public final TalonConstants constants = new TalonConstants(motorId, sensorPhase, isInverted);
+      //PID Tuning
+      public static final double kP = 0.2;
+      public static final double kI = 0.0;
+      public static final double kD = 0.0;
+      public static final double kF = 0.2;
+      public static final int kIZone = 0;
+      public static final double kNeutralDeadband = 0.001;
+      public static final double kPeakOutput = 1.0;
+      public static final double kCruise = 1000;
+      public static final double kAccel = 2000;
+      // public final PIDGains gains = new PIDGains(kP, kI, kD, kF, kIZone, kNeutralDeadband, kPeakOutput, kCruise, kAccel);
+    }      
+    public class Motor2 {
+      //Talon Tuning
+      public static final int motorId = IDs.TalonSRX.armControllerId2;
+      public static final boolean isInverted = true;
+      public static final boolean sensorPhase = false;
+      // public final TalonConstants constants = new TalonConstants(motorId, sensorPhase, isInverted);
+      //PID Tuning
+      public static final double kP = 0.2;
+      public static final double kI = 0.0;
+      public static final double kD = 0.0;
+      public static final double kF = 0.2;
+      public static final int kIZone = 0;
+      public static final double kNeutralDeadband = 0.001;
+      public static final double kPeakOutput = 1.0;
+      public static final double kCruise = 1000;
+      public static final double kAccel = 2000;
+      // public final PIDGains gains = new PIDGains(kP, kI, kD, kF, kIZone, kNeutralDeadband, kPeakOutput, kCruise, kAccel);
+    }   
+    public class Extend {
+      public static final int motorId = IDs.TalonSRX.armExtendControllerId;
+      public static final boolean isInverted = true;
+      public static final boolean sensorPhase = false;
+      // public final TalonConstants constants = new TalonConstants(motorId, sensorPhase, isInverted);
+      //PID Tuning
+      public static final double kP = 0.2;
+      public static final double kI = 0.0;
+      public static final double kD = 0.0;
+      public static final double kF = 0.2;
+      public static final int kIZone = 0;
+      public static final double kNeutralDeadband = 0.001;
+      public static final double kPeakOutput = 1.0;
+      public static final double kCruise = 1000;
+      public static final double kAccel = 2000;
+      // public final PIDGains gains = new PIDGains(kP, kI, kD, kF, kIZone, kNeutralDeadband, kPeakOutput, kCruise, kAccel);
+      public class Positions { 
+        public static final double ScoreHigh = 5000;
+        public static final double ScoreLow = 1000;
+        public static final double HPShelf = 3000;
+        public static final double Floor = 1000;
+        public static final double Home = 0;
+      }
+    }         
+  }
+
+  //Operator Interface
+  public class OI {
+    public static final int Joy = 0;
+    public static final double maxband = 0.90;
+    public static final double minband = 0.0;
+  } 
+
+  //Logitech Extreme 3d pro
+  public class Joy {
+    public static final int axis_horizontal = 0;
+    public static final int axis_vertical = 1;
+    public static final int axis_rotate = 2;
+    public static final int axis_slider = 3;
+    public static final int trigger_button = 1;
+    public static final int side_button = 2;
+    public static final int thumb3 = 3; //bottom-left
+    public static final int thumb4= 4; //bottom-right
+    public static final int thumb5 = 5; //top-left
+    public static final int thumb6 = 6; //top-right
+    public static final int lefthand7 = 7; //top-left base buttons
+    public static final int lefthand8 = 8; //top-right base buttons
+  } 
+
+  public class Controls {
+    public static final int driveShiftHigh = Joy.lefthand7;
+    public static final int driveShiftLow = Joy.lefthand8;
+    public static final int gripperActuate = Joy.trigger_button;
+    public static final int armDown = Joy.side_button;
+    public static final int armScoreHigh = Joy.thumb5;
+    public static final int armScoreLow = Joy.thumb3;
+    public static final int armHPShelf = Joy.thumb6;
+    public static final int armFloor = Joy.thumb4;
+  }
+
+}
+
   
   
     
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,24 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -25,22 +36,101 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private final WPI_VictorSPX driveLeftrear = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftrear);
-  private final WPI_VictorSPX driveLeftfront = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftfront);
-  private final WPI_TalonSRX driveTopLeft = new WPI_TalonSRX(Constants.IDs.TalonSRX.driveTopLeft);
-  private final WPI_TalonSRX driveTopRight = new WPI_TalonSRX(Constants.IDs.TalonSRX.driveTopRight);
-  private final WPI_VictorSPX driveRightrear = new WPI_VictorSPX(Constants.IDs.Victor.driveRightrear);
-  private final WPI_VictorSPX driveRightfront = new WPI_VictorSPX(Constants.IDs.Victor.driveRightfront);
+  private final WPI_VictorSPX driveLeftRear = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftrearId);
+  private final WPI_VictorSPX driveLeftFront = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftfrontId);
+  private final WPI_TalonSRX driveTopLeft = new WPI_TalonSRX(Constants.IDs.TalonSRX.driveTopLeftId);
+  private final WPI_TalonSRX driveTopRight = new WPI_TalonSRX(Constants.IDs.TalonSRX.driveTopRightId);
+  private final WPI_VictorSPX driveRightRear = new WPI_VictorSPX(Constants.IDs.Victor.driveRightrearId);
+  private final WPI_VictorSPX driveRightFront = new WPI_VictorSPX(Constants.IDs.Victor.driveRightfrontId);
   private final Joystick joy = new Joystick(Constants.OI.Joy);
-  private final MotorControllerGroup leftgroup = new MotorControllerGroup(driveTopLeft,driveLeftfront, driveLeftrear);
-  private final MotorControllerGroup rightgroup = new MotorControllerGroup(driveTopRight,driveRightfront,driveRightrear);
-  private final DifferentialDrive diff = new DifferentialDrive(leftgroup, rightgroup);
+  private final AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+  private final DifferentialDrive drive = new DifferentialDrive(driveTopLeft, driveTopRight);
+  private final DoubleSolenoid m_gearShift = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.IDs.Air.driveShiftHighId, Constants.IDs.Air.driveShiftLowId);
+  private final Solenoid m_gripper = new Solenoid(PneumaticsModuleType.REVPH, Constants.IDs.Air.gripperSolenoidId);
+  private final WPI_TalonSRX m_arm1 = new WPI_TalonSRX(Constants.IDs.TalonSRX.armControllerId1);
+  private final WPI_TalonSRX m_arm2 = new WPI_TalonSRX(Constants.IDs.Victor.armControllerId2);
+  private final WPI_TalonSRX m_extend = new WPI_TalonSRX(Constants.IDs.TalonSRX.armExtendControllerId);
+  private final Timer m_timer = new Timer();
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
+    //Setup the Talons every robot init
+    LiveWindow.disableAllTelemetry();
+
+    //Left Master
+    driveTopLeft.configFactoryDefault();
+    driveTopLeft.setInverted(Constants.DriveTrain.Left.isInverted);
+    //Left Followers
+    driveLeftFront.configFactoryDefault();
+    driveLeftFront.follow(driveTopLeft);
+    driveLeftFront.setInverted(InvertType.FollowMaster);
+    driveLeftRear.configFactoryDefault();
+    driveLeftRear.follow(driveTopLeft);
+    driveLeftRear.setInverted(InvertType.FollowMaster);
+
+    //Right Master
+    driveTopRight.configFactoryDefault();
+    driveTopRight.setInverted(Constants.DriveTrain.Right.isInverted);
+    //Right Followers
+    driveRightFront.configFactoryDefault();
+    driveRightFront.follow(driveTopRight);
+    driveRightFront.setInverted(InvertType.FollowMaster);
+    driveRightRear.configFactoryDefault();
+    driveRightRear.follow(driveTopRight);
+    driveRightRear.setInverted(InvertType.FollowMaster);
+
+    //Arm Master
+    m_arm1.configFactoryDefault();
+    m_arm1.set(ControlMode.PercentOutput, 0.0);
+    m_arm1.setNeutralMode(NeutralMode.Brake);
+    m_arm1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 30);
+    m_arm1.configNeutralDeadband(Constants.Arm.Motor1.kNeutralDeadband);
+    m_arm1.setSensorPhase(Constants.Arm.Motor1.sensorPhase);
+    m_arm1.setInverted(Constants.Arm.Motor1.isInverted);
+    m_arm1.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
+    m_arm1.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
+    m_arm1.configNominalOutputForward(0, 30);
+    m_arm1.configNominalOutputReverse(0, 30);
+    m_arm1.configPeakOutputForward(Constants.Arm.Motor1.kPeakOutput, 30);
+    m_arm1.configPeakOutputReverse(-Constants.Arm.Motor1.kPeakOutput, 30);
+    m_arm1.selectProfileSlot(0, 0); 
+    m_arm1.config_kF(0, Constants.Arm.Motor1.kF, 30);
+    m_arm1.config_kP(0, Constants.Arm.Motor1.kP, 30);
+    m_arm1.config_kI(0, Constants.Arm.Motor1.kI, 30);
+    m_arm1.config_kD(0, Constants.Arm.Motor1.kD, 30);
+    m_arm1.configMotionCruiseVelocity(Constants.Arm.Motor1.kCruise, 30);
+    m_arm1.configMotionAcceleration(Constants.Arm.Motor1.kAccel, 30);
+    //Arm Follower
+    m_arm2.configFactoryDefault();
+    m_arm2.follow(m_arm1);
+    m_arm2.setInverted(InvertType.OpposeMaster);
+    //Arm Extension 
+    m_extend.configFactoryDefault();
+    m_extend.set(ControlMode.PercentOutput, 0.0);
+    m_extend.setNeutralMode(NeutralMode.Brake);
+    m_extend.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 30);
+    m_extend.configNeutralDeadband(Constants.Arm.Motor1.kNeutralDeadband);
+    m_extend.setSensorPhase(Constants.Arm.Motor1.sensorPhase);
+    m_extend.setInverted(Constants.Arm.Motor1.isInverted);
+    m_extend.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
+    m_extend.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
+    m_extend.configNominalOutputForward(0, 30);
+    m_extend.configNominalOutputReverse(0, 30);
+    m_extend.configPeakOutputForward(Constants.Arm.Motor1.kPeakOutput, 30);
+    m_extend.configPeakOutputReverse(-Constants.Arm.Motor1.kPeakOutput, 30);
+    m_extend.selectProfileSlot(0, 0); 
+    m_extend.config_kF(0, Constants.Arm.Motor1.kF, 30);
+    m_extend.config_kP(0, Constants.Arm.Motor1.kP, 30);
+    m_extend.config_kI(0, Constants.Arm.Motor1.kI, 30);
+    m_extend.config_kD(0, Constants.Arm.Motor1.kD, 30);
+    m_extend.configMotionCruiseVelocity(Constants.Arm.Motor1.kCruise, 30);
+    m_extend.configMotionAcceleration(Constants.Arm.Motor1.kAccel, 30);
+
+    m_gearShift.set(DoubleSolenoid.Value.kForward);
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
@@ -54,7 +144,12 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("Gyro",m_gyro.getFusedHeading());
+    // SmartDashboard.putBoolean("High Gear",m_gearShift.getFwdChannel());
+    SmartDashboard.putNumber("Arm Position",m_arm1.getSelectedSensorPosition());
+    SmartDashboard.putNumber("Extend Position",m_extend.getSelectedSensorPosition());
+  }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
@@ -68,6 +163,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
@@ -94,13 +191,60 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() { 
-    diff.arcadeDrive(OI.deadband(joy.getRawAxis(1))* Constants.DriveTrain.speedmult,OI.deadband(joy.getRawAxis(0)));
+    drive.arcadeDrive(
+      OI.deadband(joy.getRawAxis(Constants.Joy.axis_vertical)) * Constants.DriveTrain.kSpeedMult,
+      OI.deadband(joy.getRawAxis(Constants.Joy.axis_horizontal)) * Constants.DriveTrain.kTurnMult
+    );
 
+    //Handle shifting the drivetrain
+    if(joy.getRawButtonPressed(Constants.Controls.driveShiftHigh)) {
+      System.out.println("teleopPeriodic: Shifting gear to High -- HIT IT CHEWWWY");
+      m_gearShift.set(DoubleSolenoid.Value.kForward);
+    } else if (joy.getRawButtonPressed(Constants.Controls.driveShiftLow)) {
+      System.out.println("teleopPeriodic: Shifting gear to Low -- I am STRONK");
+      m_gearShift.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    //Handle opening/closing the gripper
+    if(joy.getRawButtonPressed(Constants.Controls.gripperActuate)) {
+      System.out.println("teleopPeriodic: Open Gripper");
+      m_gripper.set(Constants.Gripper.kOpenState);
+    } else if (joy.getRawButtonReleased(Constants.Controls.gripperActuate)) {
+      System.out.println("teleopPeriodic: Close Gripper");
+      m_gripper.set(!Constants.Gripper.kOpenState);
+    }
+
+    //Handle moving the arm
+    if(joy.getRawButtonPressed(Constants.Controls.armDown)) {
+      System.out.println("teleopPeriodic: Arm Down");
+      m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.Home);
+      m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.Home);
+    } else if (joy.getRawButtonPressed(Constants.Controls.armScoreHigh)) {
+      System.out.println("teleopPeriodic: Scoring High");
+      m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.ScoreHigh);
+      m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.ScoreHigh);
+    } else if(joy.getRawButtonPressed(Constants.Controls.armScoreLow)) {
+      System.out.println("teleopPeriodic: Scoring Low");
+      m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.ScoreLow);
+      m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.ScoreLow);
+    } else if(joy.getRawButtonPressed(Constants.Controls.armFloor)) {
+      System.out.println("teleopPeriodic: Arm To The Floor");
+      m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.Floor);
+      m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.Floor);
+    } else if (joy.getRawButtonPressed(Constants.Controls.armHPShelf)) {
+      System.out.println("teleopPeriodic: FEED ME HUMAN");
+      m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.HPShelf);
+      m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.HPShelf);
+    }
   }
-
+  
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    m_gearShift.set(DoubleSolenoid.Value.kForward);
+    m_arm1.set(ControlMode.MotionMagic, Constants.Arm.Positions.Home);
+    m_extend.set(ControlMode.MotionMagic, Constants.Arm.Extend.Positions.Home);
+  }
 
   /** This function is called periodically when disabled. */
   @Override
